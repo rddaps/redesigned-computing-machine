58bea719309494dccf9de56d96f71f4f6f7b5772
rddaps <dapa0002@umn.edu>
2023-10-12 20:59:24 -0500

    complete back-end specifications

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,37 +1,42 @@
 const { User } = require("../models");
-const { signToken } = require('../utils/auth');
-
-
-// input BookInput {
-//   authors: [String]
-//   description: String
-//   title: String
-//   bookId: ID
-//   image: String
-//   link: String
-// }
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
     me: async ({ parent, args, context }) => {
       if (context.user) {
-        const foundUser = await User.findOne({ _id: context.user._id });
-        res.json(foundUser);
+        return User.findOne({ _id: context.user._id });
       }
-    }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
+
   Mutation: {
-    login: async (parent, { email, password  }) => {
+    login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+
       const correctPw = await user.isCorrectPassword({ password });
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong password!");
+      }
+
       const token = signToken(user);
       res.json({ token, user });
     },
-    addUser: async ( parent, { body }) => {
+
+    addUser: async (parent, { body }) => {
       const user = await User.create(body);
+      if (!user) {
+        throw new AuthenticationError("Something is wrong!");
+      }
       const token = signToken(user);
-    res.json({ token, user });
+      res.json({ token, user });
     },
+
     saveBook: async (parent, { bookInput }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -41,7 +46,9 @@ const resolvers = {
         );
         return updatedUser;
       }
+      throw new AuthenticationError("You need to be logged in!");
     },
+
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -50,7 +57,9 @@ const resolvers = {
           { new: true }
         );
         return updatedUser;
-      } 
+      }
+      throw new AuthenticationError("Couldn't find user with this id!");
+
     },
   },
 };
