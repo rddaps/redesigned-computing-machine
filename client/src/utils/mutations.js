bd5f32866ca5e8e9c9cf3b283cbb0b63233e1eac
rddaps <dapa0002@umn.edu>
2023-10-12 21:26:21 -0500

    adding execution files for queries & mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUSer($username: String!, $email: String!, $password: String!) {
+    addUser(username: $user, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: bookInput) {
+    saveBook(bookInput: $bookInput) {
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
